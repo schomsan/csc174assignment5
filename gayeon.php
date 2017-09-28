5225eecb6c2cb7b2e0b9d912d10ffd7157b8b7ad
vivss <vli4@u.rochester.edu>
2017-09-28 17:39:33 -0400

    importing base files from previous assignment

---
+++ gayeon.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html  lang="en">
+  <head>
+    <meta charset=utf-8>
+    <title> Gayeon Lee</title>
+      <link rel="stylesheet" href="css/main-styles.css" type="text/css" media="all">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  </head>
+  <body>
+  <?php include "inc/menu.inc"; ?>
+    <h1> Gayeon Lee </h1>
+
+    <div class="about-me">
+      <h2> About Me </h2>
+      <p> I am a senior computer science major at University of Rochester, Class of 2018, looking for a software development position after graduation. I am also the University of Rochester Women in Computing Web Manager.  </p>
+      <h2> Experience</h2>
+      <ul>
+        <li>Software Development Engineer Intern</li>
+        <li>Teaching Assistant</li>
+        <li>Web Design and Development</li>
+      </ul>
+  </div>
+
+
+  <div class="content">
+    <div class="experience">
+    <h3>Amazon (Seattle, WA) - Software Development Engineer Intern, Retail Systems and Services</h3>
+    <p> May – August 2017
+      Integrated an internal service’s search functionality with Elasticsearch.
+      Made first step for service’s launch in China and complete migration of search functionality to Elasticsearch
+    </p>
+  </div>
+  <div class="experience">
+    <h3> University of Rochester	(Rochester, NY) - Teaching Assistant</h3>
+    <p> September 2015 – Present
+      Teaching Assistant - Workshop leader for CSC171 The Science of Programming and CSC 172 The Science of Data Structures
+      Teaching Assistant for CSC173 Computation and Formal Systems, CSC242 Artificial Intelligence, CSC210 Web Development
+    </p>
+  </div>
+  </div>
+
+  <div class="fun-fact">
+    <h2> Fun Fact</h2>
+    <p>I am originally from South Korea, and I have been studying in the U.S. for 7 years.
+    Last summer I stayed in Seattle doing an internship, and also througout the summer
+    I visited 5 different cities - Seattle, Philadelphia, Chicago, Vancouver and New York City.</p>
+  </div>
+
+  <div class="contact">
+    <h2>Connect with me!</h2>
+    <p>Email: gayeonlee94@gmail.com</p>
+    <a href="https://www.linkedin.com/in/gayeonlee94"> LinkedIn </a>
+</div>
+    <?php include "inc/footer.inc"; ?>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Group 8 - Assignment 4</title>
+  <link rel="stylesheet" href="css/main-styles.css" type="text/css" media="all">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+</head>
+<body>
+  <?php include "inc/menu.inc"; ?>
+
+  <div class = "container">
+    <section class="overview">
+      <h1>Overview</h1>
+      <section class="people">
+        <section class="excerpt">
+          <h2>Vivian Li</h2>
+          <p>Hi! I'm an aspiring UX designer and front-end developer studying at the University of Rochester. Right now, I'm working at the ROC Human Computer Interaction Lab on campus as a research assistant where I work on developing novel user experiences. If I'm not working on new designs or coding, you'll likely find me swing dancing or biking.</p>
+          <a href=vivian.php>Read more about Vivian</a>
+        </section>
+        <section class="excerpt">
+          <h2>Ben Chomsang</h2>
+          <p>Hi! I'm Ben. I'm a student at the University of Rochester studying Film and Media and Psychology. I like trying new things, and traveling. I am a freelancer, feel free to contact me if you want to collaborate with me!</p>
+          <a href=ben.php>Read more about Ben</a>
+        </section>
+        <section class="excerpt">
+          <h2>Gayeon Lee</h2>
+          <p>I am a senior computer science major at University of Rochester, Class of 2018, looking for a software development position after graduation. I am also the University of Rochester Women in Computing Web Manager.  </p>
+          <a href=gayeon.php>Read more about Gayeon</a>
+        </section>
+      </section>
+    </section>
+  </div>
+
+  <?php include "inc/footer.inc"; ?>
+</body>
+</html>
